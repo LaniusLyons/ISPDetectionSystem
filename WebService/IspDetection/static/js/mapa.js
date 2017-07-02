93d4c1b9718ae6823944a2af40c549beba7a47d2
Cristian <metacris93@gmail.com>
2017-07-02 18:52:56 -0500

    si el usuario no acepta la ubicacion, deshabilita el poder compartir por facebook

--- WebService/IspDetection/static/js/mapa.js
+++ WebService/IspDetection/static/js/mapa.js
@@ -9,30 +9,10 @@ var map_info, map_users;
             google.maps.event.trigger(myMap, 'resize');
        }
 
-      function showError(error)
-      {
-            switch (error.code)
-            {
-                case error.PERMISSION_DENIED:
-                    alert("El requerimiento negado por el usuario");
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    alert("Informacion de localizacion no disponible");
-                    break;
-                case error.TIMEOUT:
-                    alert("Tiempo de espera expirado");
-                    break;
-                case error.UNKNOWN_ERROR:
-                    alert("ERROR DESCONOCIDO");
-                    break;
-            }
-      }
-
-      function showPosition(position)
-      {
-            // Create a Google coordinate object for where to center the map
-            var latlng = new google.maps.LatLng( position.coords.latitude, position.coords.longitude );
-            var infoOptions = {
+       function showMap(is_permite_ubicacion , position )
+       {
+           var latlng = is_permite_ubicacion ? new google.maps.LatLng( position.coords.latitude, position.coords.longitude ) : {lat: -2.16667, lng: -79.9};
+           var infoOptions = {
               zoom:12,
               mapTypeId: google.maps.MapTypeId.ROADMAP,
               center: latlng,
@@ -49,11 +29,41 @@ var map_info, map_users;
             map_users = new google.maps.Map(document.getElementById("map-users"), myOptions);
             map_users.mapTypes.set('styled_map', styledMap);
             map_users.setMapTypeId('styled_map');
-            var marker = new google.maps.Marker({
-                position: latlng,
-                map: map_info,
-                icon: 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
-            });
+            if(is_permite_ubicacion)
+            {
+                var marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map_info,
+                    icon: 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
+                });
+            }
+       }
+
+      function showError(error)
+      {
+            switch (error.code)
+            {
+                case error.PERMISSION_DENIED:
+                    //alert("El requerimiento negado por el usuario");
+                    $('#modal1').modal('open');
+                    showMap(false);
+                    $('#compartir-facebook').css("display","none");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    alert("Informacion de localizacion no disponible");
+                    break;
+                case error.TIMEOUT:
+                    alert("Tiempo de espera expirado");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    alert("ERROR DESCONOCIDO");
+                    break;
+            }
+      }
+
+      function showPosition(position)
+      {
+          showMap(true , position);
       }
 
       function initMap()
---
+++ WebService/IspDetection/static/js/modal.js
@@ -0,0 +1,14 @@
+$('#modal1').modal({
+     dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 500, // Transition in duration
+      outDuration: 500, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%' // Ending top style attribute
+      /*ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() { alert('Closed'); } // Callback for Modal close
+      */
+});
